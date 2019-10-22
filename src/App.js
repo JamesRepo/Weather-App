1e73938096839f386a089877a94fba4ef0afae54
james.elner <james.elner@polestar-eam.com>
2019-10-22 13:41:03 +0100

    initialise

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // Constructor
+  constructor(props) {
+    super(props);
+    this.state = {temp: 0, desc: '', icon: '', loading: true}
+  }
+  // REST API call
+  componentDidMount() {
+    fetch('http://api.openweathermap.org/data/2.5/weather?q=London&units=Metric&APIkey=8efe2c6a78b4b0f4e1d7324c86ea347f')
+    .then(response => response.json())
+    .then(responseData => {
+      this.setState({
+        temp: responseData.main.temp,
+        desc: responseData.weather[0].description,
+        icon: responseData.weather[0].icon,
+        loading: false
+      })
+    })
+    .catch(err => console.error(err));
+  }
+  // Show
+  render() {
+    const imgSrc = `http://openweathermap.org/img/w/${this.state.icon}.png`;
+    if (this.state.loading) {
+      return (
+        <p>Loading</p>
+      );
+    }
+    else {
+      return (
+        <div className="App">
+          <p>Temperature: {this.state.temp} C</p>
+          <p>Description: {this.state.desc}</p>
+          <img src={imgSrc} alt="Weather Icon" />     
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
