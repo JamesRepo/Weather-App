95f59a59fd743d9ea9c1ac76ac04141994fa2455
james.elner <james.elner@polestar-eam.com>
2019-10-25 16:44:16 +0100

    looks nice

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,11 +9,18 @@ class App extends Component {
   // Constructor
   constructor(props) {
     super(props);
-    this.state = {temp: 0, desc: '', icon: '', loading: true}
+    this.state = {temp: 0, desc: '', icon: '', location: 'London', loading: true, showError: false}
   }
+
+  changeLocation = (l) => {
+    this.setState({location: l.target.value});
+  }
+
   // REST API call
-  componentDidMount() {
-    fetch('http://api.openweathermap.org/data/2.5/weather?q=London&units=Metric&APIkey=8efe2c6a78b4b0f4e1d7324c86ea347f')
+  getWeather = () => {
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${this.state.location}&units=Metric&APIkey=8efe2c6a78b4b0f4e1d7324c86ea347f`;
+    console.log(url);
+    fetch(url)
     .then(response => response.json())
     .then(responseData => {
       this.setState({
@@ -20,11 +30,19 @@ class App extends Component {
         loading: false
       })
     })
-    .catch(err => console.error(err));
+    .catch(err => document.getElementById("error-message").style.display = "block");
   }
+
+  componentDidMount() {
+ //   document.getElementById("error-message").style.display = "none";
+    this.getWeather();
+    this.setState({loading: false});
+  }
+  
   // Show
   render() {
     const imgSrc = `http://openweathermap.org/img/w/${this.state.icon}.png`;
+    const error = <p>Cannot find that location</p>;
     if (this.state.loading) {
       return (
         <p>Loading</p>
@@ -33,9 +51,17 @@ class App extends Component {
     else {
       return (
         <div className="App">
-          <p>Temperature: {this.state.temp} C</p>
-          <p>Description: {this.state.desc}</p>
-          <img src={imgSrc} alt="Weather Icon" />     
+          <header className="App-header">
+            <h1>Weather</h1>
+          </header>
+          <Grid container direction="column" justify="center" alignItems="center">
+            <h3>{this.state.temp} °C</h3>
+            <h3>{this.state.desc}</h3>
+            <img src={imgSrc} alt="Weather Icon" />     
+            <TextField id="location-input" label="Location" onChange={this.changeLocation} margin="normal" variant="outlined" />
+            <Button variant="contained" onClick={this.getWeather} value={this.state.location}>Change Location</Button>
+            <p id="error-message">Cannot find that location</p>
+          </Grid>
         </div>
       );
     }
